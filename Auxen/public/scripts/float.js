f255c720a8d14f1cc90de7f1acaff852fbdd485e
mohammad-gitup <mohammadsyed.kc@gmail.com>
2017-08-05 20:45:28 -0700

    Added fire

--- Auxen/public/scripts/float.js
+++ Auxen/public/scripts/float.js
@@ -1,11 +1,9 @@
 $(document).ready(function() {
   $('#laflame').on('click', function() {
     $('#flames').append(`
-    <span style="position: relative">
       <span id="fire" class="middle" style="position: absolute">
           🔥
       </span>
-    </span>
     `)
 
     setTimeout(function() {
