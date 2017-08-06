3c87e6acd36c5c7b105f06416a0ff463fa2cfc1b
Yashvardhan Nevatia <yashvardhannevatia@Yashvardhans-MacBook-Pro.local>
2017-08-06 03:22:22 -0700

    ere

--- Auxen/routes/index.js
+++ Auxen/routes/index.js
@@ -322,6 +322,7 @@ module.exports = function(io) {
 
     /* user refreshed so adding to db */
     socket.on('userRefreshed', function(userObject){
+      console.log("user refreshed and add to database");
       Room.findById(userObject.roomId)
       .then(room => {
         var userObject = {
